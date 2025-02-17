5e932a8e247e16621da75be183a8e9613aa75196
rhythm-paudel <rhythmpaudel12@gmail.com>
2025-02-17 23:03:25 +0545

    Backend access token change in logic

--- BACKEND/controllers/refreshTokenController.js
+++ BACKEND/controllers/refreshTokenController.js
@@ -8,29 +8,33 @@ const handleRefreshToken =async (req,res)=>{
     let decryptedToken = decryptRefreshToken(encryptedToken).trim();
     console.log(decryptedToken);
     
-    const userDB = await User.findOne({ "refreshToken":decryptedToken }).exec();
-    console.log(userDB);
+   
      
     try{
-      if (!userDB) return res.sendStatus(403); //Forbidden 
+     
 
       jwt.verify(
           decryptedToken,
           process.env.REFRESH_SECRET_TOKEN,
-          (err, decoded) => {
-              if (err || userDB.email !== decoded.email) return res.status(403).json({"err":err});
+          async (err, decoded) => {
+              if (err) return res.status(403).json({"err":err});
+              const userDB = await User.findOne({ "email":decoded.email }).exec();
+              if (!userDB) return res.sendStatus(403); //Forbidden 
+              console.log(userDB);
               const accessToken = jwt.sign({
-                  "UserDetails": {
-                    "email": userDB.email,
-                    "firstname": userDB.firstname,
-                    "lastname": userDB.lastname,
-                    "dateOfBirth": userDB.dateOfBirth,
-                    "visaStamp":userDB.visaStamp
-                  }
-                },
-                  process.env.ACCESS_SECRET_TOKEN,
-                  {expiresIn:'1d'}
-                );
+                "UserDetails": {
+                  "email": userDB.email,
+                  "firstname": userDB.firstname,
+                  "lastname": userDB.lastname,
+                  "dateOfBirth": userDB.dateOfBirth,
+                  "visaVerified":userDB.visaVerified,
+                  "passportVerified":userDB.passportVerified,
+        
+                }
+              },
+                process.env.ACCESS_SECRET_TOKEN,
+                {expiresIn:'1d'}
+              );
                 res.json({accessToken })
           }
       );
