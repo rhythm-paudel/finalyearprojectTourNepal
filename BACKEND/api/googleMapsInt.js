c7e61f7bcde4479ce758da17cb299aa5b03106b9
rhythm-paudel <rhythmpaudel12@gmail.com>
2025-02-19 19:41:33 +0545

    Using Gemini AI for getting place description

--- BACKEND/api/googleMapsInt.js
+++ BACKEND/api/googleMapsInt.js
@@ -1,7 +1,15 @@
 const axios = require("axios");
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
 
 const getNearbyDestinations = async (location, radius, destinationType) => {
   const destinations = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${destinationType}&location=${location.latitude},${location.longitude}&radius=${radius}&type=${destinationType}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+
+
+  
 
   try {
     const response = await axios.get(destinations);
@@ -13,18 +21,27 @@ const getNearbyDestinations = async (location, radius, destinationType) => {
       for (const place of response.data.results){ //response.data.results.forEach this for each wont work for asynchronous method
         let latitude = place.geometry.location.lat;
         let longitude = place.geometry.location.lng;
-        getDescriptions(place.name)
+     
         
         // Only add places that have a photo reference
-
         let photo_aaa =
           place.photos && place.photos.length ? await getPhoto(place) : false;
 
+        //declaring description variable
+        let descriptionOfPlace = ""
+
+
+        if(destinationType==="tourist_attraction"){  
+          descriptionOfPlace = await getDescription(place.name);
+        }else{
+          descriptionOfPlace = "";
+        }
         placesDict.push({
           name: place.name,
           location: { latitude: latitude, longitude: longitude },
           rating: place.rating,
           photoRef: photo_aaa,
+          description: descriptionOfPlace
         });
        
       };
@@ -37,21 +54,7 @@ const getNearbyDestinations = async (location, radius, destinationType) => {
   }
 };
 
-const getDescriptions = async (placeName) => {
-  try{
-    const desc = await axios.get(`https://en.wikipedia.org/api/rest_v1/page/summary/Patan Darbar Square`)
-    if(desc.data){
-      return desc.data.extract;
-      
-    }
-    else{
-      return false
-      
-    }
-  }catch(err){
-    return false
-  }
-};
+
 
 const getPhoto = async (place) => {
   const  photo_aaa = await axios.get(
@@ -66,5 +69,39 @@ const getPhoto = async (place) => {
   }
   return false;
 };
+
+
+const getDescription = async (placeName)=>{
+  
+  
+  const apiKey = process.env.GOOGLE_GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run() {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(`Provide me a short description in about 4 sentences about ${placeName}. If nothing is found simple
+                                                  return empty string as a message. The description should be short and concise`);
+    console.log(result.response.text());
+  }
+  
+  run();
+}
 
 module.exports = { getNearbyDestinations };
